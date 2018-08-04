816393aab784ff185ea3d9b8c1133e30b70cdf37
Ubuntu <ubuntu@ip-172-31-33-172.us-east-2.compute.internal>
2018-08-04 15:10:40 +0000

    Fixed Timeout Issues for good. (hopefully)

--- GetPhotos.py
+++ GetPhotos.py
@@ -43,7 +43,6 @@ USERS = ['@mlstylephoto']
 
 while True:
 	try:
-<<<<<<< HEAD
 		for line in api.GetStreamFilter(track=USERS):
     			tweet = line
     			media = tweet.get('extended_entities', {}).get('media', [])
@@ -59,17 +58,14 @@ while True:
         			url_1 = str(pic[0].encode("utf-8"))
         			url_2 = str(pic[1].encode("utf-8"))
 			try:
+				conn = pymysql.connect(rds_host, user=name, passwd=password, db=db_name, connect_timeout=5)
+				with conn.cursor() as cur:
+                               		cur.execute('insert into Queue (Time, Picture, StylePic, Username, Tweet_ID, Complete) values("'+time+'","'+url_1+'","'+url_2+'","'+str(user)+'","'+str(tweet_id)+'","'+str(complete)+'")')
+                                	conn.commit()
 				ec2 = boto3.client('ec2', region_name = 'us-east-2')
 				ec2.start_instances(InstanceIds=['i-0bf21239e1c02ee77'])
-				conn = pymysql.connect(rds_host, user=name, passwd=password, db=db_name, connect_timeout=5)
+				conn.close()
 			except:
 				pass
-        		with conn.cursor() as cur:
-            			cur.execute('insert into Queue (Time, Picture, StylePic, Username, Tweet_ID, Complete) values("'+time+'","'+url_1+'","'+url_2+'","'+str(user)+'","'+str(tweet_id)+'","'+str(complete)+'")')
-            			conn.commit()
-=======
-		ec2 = boto3.client('ec2', region_name = 'us-east-2')
-		ec2.start_instances(InstanceIds=['i-############'])
->>>>>>> 39d3a723c8e50a293f8beeee8b1972efa390cc95
 	except:
 		continue
